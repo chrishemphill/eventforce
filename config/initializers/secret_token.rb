3fbc9943dd188ddfed7445180cee1653310e996b
Chris Hemphill <chris@chrishemphill.com>
2011-02-02 17:42:08 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventforce::Application.config.secret_token = '926de45c4379f5e37a402c92f915087eceda175eee89e3fd761ae4e178362f47976ad880aae13c2a4f266e4eb3649070b23f2488ba4d2ee7ea04f92630675c16'
